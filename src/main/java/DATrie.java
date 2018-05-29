327867b9a3ec5738f75c404e870d9e668414f705
Koma <zhangqiang@easemob.com>
2018-05-29 16:01:16 +0800

    重构代码:代码组织格式调整,必要注释增加

--- src/main/java/DATrie.java
+++ src/main/java/DATrie.java
@@ -3,9 +3,13 @@ import java.util.List;
 
 public class DATrie {
     private final int ARRAY_SIZE = 655350;
-    private final int ROOT_BASE = 1;
-    private final int ROOT_CHECK_BASE = -1;
-    private final int BASE_NULL = ARRAY_SIZE+1;
+    private final int BASE_ROOT = 1;
+    //根据状态转移公式：|base[start]|+code=end,可知当code>0时,|base[start]| < end
+    //即： -end < base[start] < end，其中由于 base 数组中 0 作为根节点，则 end 最小值可取为 1
+    //当 end 取 1 时则 base[start] = 0，则 code+1=1 得 code = 0，这和已知条件矛盾,因此可知
+    //base数组中，闲置位置默认值可设置为: 0
+    private final int BASE_NULL = 0;
+    private final int CHECK_ROOT = -1;
     private final int CHECK_NULL = -2;
     private int base[];
     private int check[];
@@ -14,7 +18,7 @@ public class DATrie {
         init();
 
         int pos = 0;
-        for (int c = 0; c < words.size(); c++) {
+        for (int idx = 0; idx < words.size(); idx++) {
             for (String word : words) {
                 char chars[] = word.toCharArray();
                 if (chars.length > pos) {
@@ -29,26 +33,56 @@ public class DATrie {
         }
     }
 
-    public void debug() {
+    public List<String> match(String keyWord) {
+        List<String> result = new ArrayList<String>();
+        List<Character> characterList;
+        int startState, endState;
+
+        char chars[] = keyWord.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            startState = 0;
+            characterList = new ArrayList<Character>();
+            for (int j = i; j < chars.length; j++) {
+                endState = transfer(startState, getCode(chars[j]));
+                if (base[endState] != BASE_NULL && check[endState] == startState) { //节点存在于 Trie 树上
+                    characterList.add(chars[j]);
+                    if (base[endState] < 0 && base[endState] == base[startState]*-1) { //叶子节点
+                        StringBuilder sb = new StringBuilder();
+                        for (Character c : characterList) {
+                            sb.append(c);
+                        }
+                        result.add(sb.toString());
+                    }
+                    startState = endState;
+                } else {
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public void showTrie() {
         System.out.println();
         System.out.printf("%5s", "idx");
         for (int i = 0; i < ARRAY_SIZE; i++) {
             if (base[i] != BASE_NULL) {
-                System.out.printf("%7d", i);
+                System.out.printf("%7d\t", i);
             }
         }
         System.out.println();
         System.out.printf("%5s", "base");
         for (int i = 0; i < ARRAY_SIZE; i++) {
             if (base[i] != BASE_NULL) {
-                System.out.printf("%7d", base[i]);
+                System.out.printf("%7d\t", base[i]);
             }
         }
         System.out.println();
         System.out.printf("%5s", "check");
         for (int i = 0; i < ARRAY_SIZE; i++) {
             if (base[i] != BASE_NULL) {
-                System.out.printf("%7d", check[i]);
+                System.out.printf("%7d\t", check[i]);
             }
         }
         System.out.println();
@@ -75,44 +109,13 @@ public class DATrie {
         if (isLeaf) {
             base[endState] = Math.abs(base[startState])*-1; //叶子节点转移基数标识为父节点转移基数的相反数
         } else {
-            if (base[endState] == BASE_NULL) {
-                base[endState] = Math.abs(base[startState]);
+            if (base[endState] == BASE_NULL) { //未有节点经过
+                base[endState] = Math.abs(base[startState]); //非叶子节点的转移基数一定为正
             }
         }
         check[endState] = startState;//check中记录当前状态的父状态
 
         return endState;
-    }
-
-    public List<String> match(String keyWord) {
-        List<String> result = new ArrayList<String>();
-        List<Character> characterList;
-        int startState, endState;
-
-        char chars[] = keyWord.toCharArray();
-        for (int i = 0; i < chars.length; i++) {
-            startState = 0;
-            characterList = new ArrayList<Character>();
-            for (int j = i; j < chars.length; j++) {
-                endState = transfer(startState, getCode(chars[j]));
-                if (base[endState] != BASE_NULL && check[endState] == startState) {
-                    characterList.add(chars[j]);
-                    if (base[endState] < 0 && base[endState] == base[startState]*-1) {
-                        //叶子节点
-                        StringBuilder sb = new StringBuilder();
-                        for (Character c : characterList) {
-                            sb.append(c);
-                        }
-                        result.add(sb.toString());
-                    }
-                    startState = endState;
-                } else {
-                    break;
-                }
-            }
-        }
-
-        return result;
     }
 
     /**
@@ -138,7 +141,7 @@ public class DATrie {
             base[i] = BASE_NULL;
             check[i] = CHECK_NULL;
         }
-        base[0] = ROOT_BASE;
-        check[0] = ROOT_CHECK_BASE;
+        base[0] = BASE_ROOT;
+        check[0] = CHECK_ROOT;
     }
 }
--- src/main/java/DATrieTest.java
+++ src/main/java/DATrieTest.java
@@ -7,17 +7,28 @@ import java.util.Set;
 
 public class DATrieTest extends TestCase {
     public void testDATrie() {
+        //定义词典
         List<String> words = new ArrayList<String>();
-        words.add("清华");
-        words.add("清华大学");
-        words.add("清新");
-        words.add("中华");
-        words.add("中华人民");
-        words.add("华人");
-        words.add("学生");
-        words.add("大学生");
+//        words.add("清华");
+//        words.add("清华大学");
+//        words.add("清新");
+//        words.add("中华");
+//        words.add("中华人民");
+//        words.add("华人");
+//        words.add("学生");
+//        words.add("大学生");
 
-        //先制作一份码表，以备验证
+        words.add("qin");
+        words.add("shi");
+        words.add("ming");
+        words.add("yue");
+        words.add("zhi");
+        words.add("jun");
+        words.add("lin");
+        words.add("tian");
+        words.add("xia");
+
+        //制作码表，以备验证
         Set<Character> codes = new HashSet<Character>();
         for (String word : words) {
             char chars[] = word.toCharArray();
@@ -26,24 +37,34 @@ public class DATrieTest extends TestCase {
             }
         }
         for (Character character : codes) {
-            System.out.printf("%6s", character.charValue());
+            System.out.printf("%6s\t", character.charValue());
         }
         System.out.println();
         for (Character character : codes) {
-            System.out.printf("%7d",  (int)character.charValue());
+            System.out.printf("%6d\t",  (int)character.charValue());
         }
         System.out.println();
 
+        //构建 Trie 树
         DATrie daTrie = new DATrie();
         daTrie.build(words);
-        daTrie.debug();
+        daTrie.showTrie();
 
+        //执行匹配
+//        List<String> result = daTrie.match("清华大学生都是华人");
+        List<String> result = daTrie.match("qinshimingyuezhijunlintianxia");
+
+        //打印匹配结果
         System.out.println();
-        List<String> result = daTrie.match("清华大学生都是华人");
-        System.out.printf("%5s", "Match");
-        for (String s : result) {
-            System.out.printf("%6s", s);
+        System.out.printf("Match: {");
+        for (int i = 0; i < result.size(); i++) {
+            if (i == 0) {
+                System.out.printf("%s", result.get(i));
+            } else {
+                System.out.printf(", %s", result.get(i));
+            }
         }
+        System.out.printf("}");
         System.out.println();
     }
 }
